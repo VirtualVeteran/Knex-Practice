1c1829e8518d4d98265528281ff58e9984a5e48b
VirtualVeteran <estheroswald001@yahoo.com>
2024-04-02 12:35:46 -0600

    creates express

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://postgres:docker@172.17.0.2:5432/pg-docker'
+  
+    
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};

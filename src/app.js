dafadbf2f6ebd74959f35d9d096ea950c8f8cc5a
VirtualVeteran <estheroswald001@yahoo.com>
2024-04-02 15:32:17 -0600

    adds path to development in knex

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
+
 const express = require('express');
+
 const app = express();
 const port = 8081;
+
+const knex = require('knex')(require('../knexfile.js')["development"]);
+
+app.get('/', (request ,response) => {
+    response.send('application up and running');
+});
+
+
+app.listen(port, () => {
+  console.log(`The server is running`);
+});
+
+app.get('/fairys', (request, response) => {
+    knex('fairy')
+        .select('*')
+        .then(fairys => {
+            var fairyNames = fairys.map(fairy => fairy.name)
+            response.json(fairyNames);
+        })
+})

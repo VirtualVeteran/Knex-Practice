667e2f27b1ba35ceedd43c04d09fefb9b1a37de1
VirtualVeteran <estheroswald001@yahoo.com>
2024-04-02 14:24:11 -0600

    creates fairy talent table

---
+++ seeds/fairy_type.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('fairy_type').del()
+  await knex('fairy_type').insert([
+    {id: 1, name: 'AnimalTalent'},
+    {id: 2, name: 'WaterTalent'},
+    {id: 3, name: 'GardenTalent'},
+    {id: 4, name: 'TinkerTalent'}
+  ]);
+};

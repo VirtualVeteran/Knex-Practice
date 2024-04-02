a6b404c725c2e8f7bec03e31307054dc65f9b88c
VirtualVeteran <estheroswald001@yahoo.com>
2024-04-02 16:15:27 -0600

    creates .env path and index.js

--- seeds/02_fairy_data_population.js
+++ seeds/02_fairy_data_population.js
@@ -4,12 +4,13 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('fairy').del()
+  await knex('fairy').del();
+  
+  // Inserts seed entries
   await knex('fairy').insert([
-    {id: 1, name: 'Tinkerbelle', fairy_type_id:4},
-    {id: 2, name: 'Fawn', fairy_type_id: 1},
-    {id: 3, name: 'Rani', fairy_type_id: 2},
-    {id: 4, name: 'Rosetta', fairy_type_id: 3}
-
+    {id: 1, name: 'Tinkerbelle', fairy_type_id:4, picture_url: 'https://example.com/tinkerbelle.jpg'},
+    {id: 2, name: 'Fawn', fairy_type_id: 1, picture_url: 'https://example.com/fawn.jpg'},
+    {id: 3, name: 'Rani', fairy_type_id: 2, picture_url: 'https://example.com/rani.jpg'},
+    {id: 4, name: 'Rosetta', fairy_type_id: 3, picture_url: 'https://example.com/rosetta.jpg'}
   ]);
 };
